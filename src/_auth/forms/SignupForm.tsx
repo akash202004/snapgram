69d7bc5249a64420549252fc0d24bdcfb235c77b
akash202004 <akashlaha48@gmail.com>
2024-05-25 08:15:19 +0530

    Add new files and components

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default SignupForm;
